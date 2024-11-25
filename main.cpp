dfe6c3d1022ee2378cc48dd136c9c8b9b07ca2f7
a01713204 <a01713204@tec.mx>
2024-11-24 23:37:26 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,14 +4,21 @@
 #include "Partido.h"
 using namespace std;
 int main() {
+    Equipo equipo;
     // Mostrar cada clase
-    Jugador jugador1("Pablo Torres", 19, "Delantero", 5000);
-    Entrenador entrenador1("Pep Guardiola", 15.8, 1000, 4);
+    equipo.agregarJugador("Pablo Torres", 5000, "Delantero", 19);
+    equipo.agregarJugador("Leo Messi", 100000, "Delantero", 37);
+    equipo.agregarJugador("Cristiano Ronaldo", 1000, "Delantero", 39);
+    equipo.agregarJugador("Lamine Yamal", 60455, "Extremo derecho", 17);Entrenador entrenador("15 años", 4, "Pep Guardiola", 15000);
     Partido partido1("Tec Qro", 3, 2);
+    Partido partido2("Real Madrid", 8, 2);
+    Partido partido3("Barcelona", 3, 8);
+    Partido partido4("Manchester City", 3, 3);
 
-    jugador1.imprime_datos();
-    entrenador1.imprime_datos();
-    partido1.imprime_datos();
+    Entrenador entrenador("Pep Guardiola", 5000, "5 años", 30);
+    equipo.agregarPartido("Tec Qro", 3, 2);
+    equipo.agregarPartido("Real Madrid", 8, 2);
 
+    equipo.verEquipo();
     return 0;
 }

c93e88f8fe37aef88aa2783a6d435e9382e02c9e
a01713204 <a01713204@tec.mx>
2024-11-24 23:34:14 -0600

    Update Jugador.h

--- Jugador.h
+++ Jugador.h
@@ -1,54 +1,47 @@
 #ifndef JUGADOR_H
-#define JUGADOR_H //Nos sirve de prevención
-#include <iostream> //el # es para importar una librería
+#define JUGADOR_H 
+#include "Miembro.h"
+#include <iostream> 
 using namespace std;
-class Jugador {
+class Jugador:public Miembro {
     
 private:
-    string nombre;
     float edad;   // años
     string posicion;
-    float salario; //pesos
 
 public:
     // Constructor por omisión o defecto
-    Jugador() {
-        nombre = "desconocido";
+    Jugador(): Miembro(){
         edad = 0;
         posicion = "desconocida";
-        salario=0;
 
     }
 
     // Constructor de parámetros
-    Jugador(string _nombre, float _edad, string _posicion, float _salario) {
-        nombre = _nombre;
+    Jugador(float _edad, string _posicion, string _nombre, float _salario): Miembro(_nombre, _salario) {
         edad = _edad;
         posicion = _posicion;
-        salario= _salario;
     }
     
-    string get_Nombre() { 
-        return nombre; 
-        }
-    float get_Edad() { 
+    float getEdad() { 
         return edad;
         }
-    string get_Posicion() { 
+    string getPosicion() { 
         return posicion; 
         }
-    float get_Salario() { 
-        return salario; 
-        }
-
     
-    void set_Nombre(string _nombre) {nombre = _nombre; }
-    void set_Edad(float _edad) {edad = _edad; }
-    void set_Posicion(string _posicion) { posicion = _posicion; }
-    void set_Salario(float _salario) { salario = _salario; }
+    void setEdad(float _edad) {
+        edad = _edad;
+         }
+    void setPosicion(string _posicion) {
+         posicion = _posicion; 
+         }
 
     void imprime_datos(){
-        cout << "Jugador: " << nombre << ", Edad: " << edad << ", Posición" << posicion<< ". Salario: $" << salario<<endl;
+        cout << "Jugador: "<< endl;
+        Miembro::imprime_datos();
+        cout<< "Edad: " << edad<< endl;
+        cout <<", Posición" << posicion<< endl;
     }
 
 };

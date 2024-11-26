a53c95b180bf5c509bf1104197e9dad15c5a7af4
a01713204 <a01713204@tec.mx>
2024-11-26 10:28:12 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,18 @@
 #include <iostream> //el # es para importar una librería
-#include "Jugador.h"
-#include "Entrenador.h"
-#include "Partido.h"
+#include "Equipo.h"
 using namespace std;
 int main() {
     Equipo equipo;
     // Mostrar cada clase
-    equipo.agregarJugador("Pablo Torres", 5000, "Delantero", 19);
-    equipo.agregarJugador("Leo Messi", 100000, "Delantero", 37);
-    equipo.agregarJugador("Cristiano Ronaldo", 1000, "Delantero", 39);
-    equipo.agregarJugador("Lamine Yamal", 60455, "Extremo derecho", 17);Entrenador entrenador("15 años", 4, "Pep Guardiola", 15000);
-    
+    equipo.agregarJugador("Pablo Torres", 5000, 19, "Delantero");
+    equipo.agregarJugador("Leo Messi", 100000, 37,"Delantero");
+    equipo.agregarJugador("Cristiano Ronaldo", 1000, 39,"Delantero");
+    equipo.agregarJugador("Lamine Yamal", 60455, 17, "Extremo derecho");
 
     Entrenador entrenador("Pep Guardiola", 5000, "5 años", 30);
     equipo.agregarPartido("Tec Qro", 3, 2);
     equipo.agregarPartido("Real Madrid", 8, 2);
 
-    equipo.verEquipo();
+    equipo.ver_equipo();
     return 0;
 }

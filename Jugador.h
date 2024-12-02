0c530a063ce80d21fe151ce825ab28d35988059a
a01713204 <a01713204@tec.mx>
2024-12-01 21:54:27 -0600

    Update Jugador.h

--- Jugador.h
+++ Jugador.h
@@ -3,7 +3,7 @@
 #include "Miembro.h"
 #include <iostream> 
 using namespace std;
-class Jugador:public Miembro {
+class Jugador:public Miembro { //Se incluye la clase miembro, ya que tiene atributos de la misma
     
 private:
     float edad;   // años
@@ -11,35 +11,35 @@ private:
 
 public:
     // Constructor por omisión o defecto
-    Jugador(): Miembro(){
+    Jugador(): Miembro(){ //nuestra clase miembro que comparte atributos con jugador
         edad = 0;
         posicion = "desconocida";
 
     }
 
-    // Constructor de parámetros
+    // Constructor de parámetros de la clase jugador y la clase miembro
     Jugador(string _nombre, float _salario, float _edad, string _posicion): Miembro(_nombre, _salario) {
         edad = _edad;
         posicion = _posicion;
     }
-    
+    //getters
     float getEdad() { 
         return edad;
         }
     string getPosicion() { 
         return posicion; 
         }
-    
+    //setters
     void setEdad(float _edad) {
         edad = _edad;
          }
     void setPosicion(string _posicion) {
          posicion = _posicion; 
          }
-
+    //Mostramos los datos de jugador
     void imprimeDatos(){
         cout << "Jugador: "<< endl;
-        Miembro::imprimeDatos();
+        Miembro::imprimeDatos(); //Se aplicará el "imprimeDatos" que se implementó en la clase Miembro
         cout<< "Edad: " << edad<< endl;
         cout <<", Posición" << posicion<< endl;
     }
